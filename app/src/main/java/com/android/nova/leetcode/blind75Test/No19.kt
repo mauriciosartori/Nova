de9f66437db5d8d10df11050fe2a9e7a610d8e11
Mauricio Sartori <omauricioguerrero@gmail.com>
2024-08-21 17:07:38 -0700

    chore: add extra practice

---
+++ app/src/main/java/com/android/nova/leetcode/blind75Test/No19.kt
@@ -0,0 +1,22 @@
+package com.android.nova.leetcode.blind75Test
+/**
+ *
+ */
+class No19 {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val result = reverseBits(43261596)
+            println("The result is actually  $result")
+        }
+        fun reverseBits(n:Int):Int {
+            var temp = n
+            var result = 0
+            for (i in 0 until 32) {
+                val bit = temp shr i and 1
+                result = result or (bit shl (31 - i))
+            }
+            return result
+        }
+    }
+}
