5dfcfe514a7cb018638b9550496cbb7c12c4c17e
Mauricio Sartori <omauricioguerrero@gmail.com>
2024-09-16 14:56:39 -0700

    chore: add daily practice

--- app/src/main/java/com/android/nova/leetcode/blind75Test/dailypractice/No20.kt
+++ app/src/main/java/com/android/nova/leetcode/blind75Test/dailypractice/No20.kt
@@ -6,31 +6,50 @@ class No20 {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val N = 4
-            val L = arrayOf(4, 1, 2, 1)  // Adjusted to 0-based indexing
+            // First input
+            var N = 4
+            var L = arrayOf(4, 1, 2, 1)
 
-            // Create an instance of No20 and call the function
-            val result = getMaxVisitableWebpages(N, L)
-            println("Max visitable webpages: $result")
+            // Call the function with the first set of inputs
+             var result = getMaxVisitableWebpages(N, L)
+            println("Max visitable webpages for  EXPECTED 4 : $result")
+
+            // Second input
+            N = 5
+            L = arrayOf(4, 3, 5, 1, 2)
+
+            // Call the function with the second set of inputs
+            var resultado = getMaxVisitableWebpages(N, L)
+            println("Max visitable webpages for EXPECTED 3 : $resultado")
         }
         // Write any import statements here
 
         fun getMaxVisitableWebpages(N: Int, L: Array<Int>): Int {
             val chaptersList = L
+            val memoMap = HashMap<Pair<Int, Int>, Int>()
+            var lastValueAdded = 0
 
             fun dfs(chapterIndex: Int, counter:Int, setVisited: HashSet<Int>): Int {
                 if (setVisited.contains(chapterIndex)) {
                     return counter
                 }
                 val nextChapterIndex = (chaptersList[chapterIndex] - 1)
+                val memoPair = Pair(chapterIndex, nextChapterIndex)
+                if (memoMap.contains(memoPair) && !setVisited.contains(nextChapterIndex)) {
+                    return memoMap.getOrDefault(memoPair, 1) + counter
+                }
                 setVisited.add(chapterIndex)
+                lastValueAdded = chapterIndex
+                val result = dfs(nextChapterIndex, counter + 1, setVisited)
 
-                return dfs(nextChapterIndex, counter + 1, setVisited)
+                return result
             }
 
             var count = 0
-            for (nextIndex in chaptersList) {
-                val steps = dfs(nextIndex, 1, HashSet<Int>())
+            for (index in chaptersList.indices) {
+                val steps = dfs(index, 0, HashSet<Int>())
+                val memoPair = Pair(index, lastValueAdded)
+                memoMap[memoPair] = steps
                 count = maxOf(count, steps)
             }
 
