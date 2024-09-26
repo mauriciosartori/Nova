a71ed28bf88ee181b5a67b8918ef9f9e769601d1
Mauricio Sartori <omauricioguerrero@gmail.com>
2024-09-25 22:50:47 -0700

    chore: add more practice

--- app/src/main/java/com/android/nova/leetcode/blind75Test/dailypractice/No47.kt
+++ app/src/main/java/com/android/nova/leetcode/blind75Test/dailypractice/No47.kt
@@ -6,10 +6,26 @@ class No47 {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            println("")
+//            val s = "ABAB"
+//            val k = 2
+            val s = "AABABBA"
+            val k = 1
+            println(characterReplacement(s, k))
         }
-        fun longestCommonSubsequence(text1: String, text2: String): Int {
-
+        fun characterReplacement(s: String, k: Int): Int {
+            val input = s
+            val memo = HashMap<Char, Int>()
+            var i = 1
+            var maxLength = 1
+            for (char in input) {
+                var valor = memo.getOrDefault(char, 1)
+                if (i - valor <= k) {
+                    maxLength = maxOf(maxLength, valor + k)
+                }
+                memo[char] = memo.getOrDefault(char, 1) + 1
+                i++
+            }
+            return maxLength
         }
     }
 }
